ca59f1bb8efe3d3ccad32f268fb56631e765cd37
nqlp <nqlpaul@gmail.com>
2024-04-05 09:54:39 -0400

    Fix formatting issue in script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let playerScore = 0;
     let computerScore = 0;
-    
+
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
