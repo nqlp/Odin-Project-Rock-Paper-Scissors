1e8e4de5a7210cd38ea3396f5c742236105a1ccc
nqlp <nqlpaul@gmail.com>
2024-04-18 11:00:58 -0400

    add a period to the sentence

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 playerScore++;
                 document.getElementById("playerScore").textContent = "Player Score: " + playerScore;
                 document.getElementById("computerChoice").textContent = computerSelection;
-                document.getElementById("winner").textContent = "You. 🎉 " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1) + " beats " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1);
+                document.getElementById("winner").textContent = "You. 🎉 " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1) + " beats " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1)+ ".";
                 checkWinner();
 
             } else {
                 computerScore++;
                 document.getElementById("computerScore").textContent = "Computer Score: " + computerScore;
                 document.getElementById("computerChoice").textContent = computerSelection;
-                document.getElementById("winner").textContent = "The computer. " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1) + " beats " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1);
+                document.getElementById("winner").textContent = "The computer. " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1) + " beats " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1) + ".";
                 checkWinner();
 
             }
