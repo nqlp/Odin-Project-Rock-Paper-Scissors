9b7d475495c0b9ae34f06d81663c16226aa377e2
nqlp <nqlpaul@gmail.com>
2024-04-20 16:41:53 -0400

    format document

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 playerScore++;
                 document.getElementById("playerScore").textContent = "Player Score: " + playerScore;
                 document.getElementById("computerChoice").textContent = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-                document.getElementById("winner").textContent = "You. 🎉 " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1) + " beats " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1)+ ".";
+                document.getElementById("winner").textContent = "You. 🎉 " + `${playerSelection}`.charAt(0).toUpperCase() + `${playerSelection}`.slice(1) + " beats " + `${computerSelection}`.charAt(0).toUpperCase() + `${computerSelection}`.slice(1) + ".";
                 checkWinner();
 
             } else {
